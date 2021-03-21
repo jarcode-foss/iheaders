597d705c64f4ea20ee074b5aa34c9b5ee85055a6
Jarcode <wacossusca34@gmail.com>
2021-03-21 14:14:15 -0700

    Perform checksum comparison on header destinations to restore access times

--- iheaders.c
+++ iheaders.c
@@ -35,6 +35,7 @@
 
 #include <getopt.h>
 
+#include <fcntl.h>
 #include <unistd.h>
 
 #include <sys/stat.h>
@@ -63,8 +64,8 @@ static const char* help_desc =
 /* '\1' defines the start of the description, '\2' indicates a new indented line. */
 static const char* help_opts =
     "-h, --help\1show this help and exit\n"
-    "-p, --strip\1strip header information from the source files, producing C sources\2"
-    "that can be compiled.\n"
+    "-p, --strip\1strip header information from the source files, producing C\2"
+    "sources that can be compiled.\n"
     "-v, --verbose\1show detailed information about inline header processing\n"
     "-t, --token=WORD\1sets the token for the processing rules to the specified string\n"
     "-d, --header-dir=PATH\1defines the directory for headers to be placed into\n"
@@ -76,6 +77,9 @@ static const char* help_opts =
     "-G, --include-gaurds\1place include gaurds in the resulting header file(s)\n"
     "-P, --file-prefix\1sets the prefix used for all output files\n"
     "-S, --file-suffix\1sets the suffix used for all output files, before the extension\n"
+    "-K, --skip-checksum\1skips performing a checksum on destination files for preserving\2"
+    "file access/modification times. The default behaviour is\2"
+    "useful for build tools that track header dependencies.\n"
     "-I, --tab-indent=SIZE\1defines the amount of spaces that a tab occupies, affecting how\2"
     "header block (@ { ... } syntax) indentation is copied to\2"
     "the resulting header file. Set to 0 to preserve all\2"
@@ -88,7 +92,7 @@ static const char* help_footer = "\n" /* padding from the option list */
     "a single header, and pipe mode ('-O' option) - similar to single-header mode, except\n"
     "the resulting file is piped to stdout.\n\n";
 
-static const char* opt_str = "hvps:t:d:r:I:OGP:S:";
+static const char* opt_str = "hvps:t:d:r:I:OGP:S:K";
 
 static struct option p_opts[] = {
     {"help", no_argument, 0, 'h'},
@@ -102,9 +106,272 @@ static struct option p_opts[] = {
     {"file-prefix", required_argument, 0, 'P'},
     {"file-suffix", required_argument, 0, 'S'},
     {"tab-indent", required_argument, 0, 'I'},
+    {"skip-checksum", no_argument, 0, 'K'},
     {"stdout", no_argument, 0, 'O'},
     {0, 0, 0, 0}
 };
+
+/* START SHA-256 IMPLEMENTATION */
+
+/*
+ * FIPS-180-2 compliant SHA-256 implementation
+ *
+ * Copyright (C) 2001-2003  Christophe Devine
+ *
+ * [Note: released under GPLv2 (and later)]
+ * source: https://casper.ssl.berkeley.edu/svn/trunk/roach/sw/uboot/lib_generic/sha256.c
+ */
+
+/*
+ * 32-bit integer manipulation macros (big endian)
+ */
+typedef struct {
+    uint32_t total[2];
+    uint32_t state[8];
+    uint8_t buffer[64];
+} sha256_context;
+
+#ifndef GET_UINT32_BE
+#define GET_UINT32_BE(n,b,i) {				\
+	(n) = ( (unsigned long) (b)[(i)    ] << 24 )	\
+	    | ( (unsigned long) (b)[(i) + 1] << 16 )	\
+	    | ( (unsigned long) (b)[(i) + 2] <<  8 )	\
+	    | ( (unsigned long) (b)[(i) + 3]       );	\
+}
+#endif
+#ifndef PUT_UINT32_BE
+#define PUT_UINT32_BE(n,b,i) {				\
+	(b)[(i)    ] = (unsigned char) ( (n) >> 24 );	\
+	(b)[(i) + 1] = (unsigned char) ( (n) >> 16 );	\
+	(b)[(i) + 2] = (unsigned char) ( (n) >>  8 );	\
+	(b)[(i) + 3] = (unsigned char) ( (n)       );	\
+}
+#endif
+
+static void sha256_starts(sha256_context * ctx) {
+	ctx->total[0] = 0;
+	ctx->total[1] = 0;
+
+	ctx->state[0] = 0x6A09E667;
+	ctx->state[1] = 0xBB67AE85;
+	ctx->state[2] = 0x3C6EF372;
+	ctx->state[3] = 0xA54FF53A;
+	ctx->state[4] = 0x510E527F;
+	ctx->state[5] = 0x9B05688C;
+	ctx->state[6] = 0x1F83D9AB;
+	ctx->state[7] = 0x5BE0CD19;
+}
+
+static void sha256_process(sha256_context * ctx, uint8_t data[64]) {
+	uint32_t temp1, temp2;
+	uint32_t W[64];
+	uint32_t A, B, C, D, E, F, G, H;
+
+	GET_UINT32_BE(W[0], data, 0);
+	GET_UINT32_BE(W[1], data, 4);
+	GET_UINT32_BE(W[2], data, 8);
+	GET_UINT32_BE(W[3], data, 12);
+	GET_UINT32_BE(W[4], data, 16);
+	GET_UINT32_BE(W[5], data, 20);
+	GET_UINT32_BE(W[6], data, 24);
+	GET_UINT32_BE(W[7], data, 28);
+	GET_UINT32_BE(W[8], data, 32);
+	GET_UINT32_BE(W[9], data, 36);
+	GET_UINT32_BE(W[10], data, 40);
+	GET_UINT32_BE(W[11], data, 44);
+	GET_UINT32_BE(W[12], data, 48);
+	GET_UINT32_BE(W[13], data, 52);
+	GET_UINT32_BE(W[14], data, 56);
+	GET_UINT32_BE(W[15], data, 60);
+
+    #define SHR(x,n) ((x & 0xFFFFFFFF) >> n)
+    #define ROTR(x,n) (SHR(x,n) | (x << (32 - n)))
+
+    #define S0(x) (ROTR(x, 7) ^ ROTR(x,18) ^ SHR(x, 3))
+    #define S1(x) (ROTR(x,17) ^ ROTR(x,19) ^ SHR(x,10))
+
+    #define S2(x) (ROTR(x, 2) ^ ROTR(x,13) ^ ROTR(x,22))
+    #define S3(x) (ROTR(x, 6) ^ ROTR(x,11) ^ ROTR(x,25))
+
+    #define F0(x,y,z) ((x & y) | (z & (x | y)))
+    #define F1(x,y,z) (z ^ (x & (y ^ z)))
+
+    #define R(t)                                \
+        (                                       \
+            W[t] = S1(W[t - 2]) + W[t - 7] +	\
+            S0(W[t - 15]) + W[t - 16]           \
+            )
+
+    #define P(a,b,c,d,e,f,g,h,x,K) {                \
+            temp1 = h + S3(e) + F1(e,f,g) + K + x;	\
+            temp2 = S2(a) + F0(a,b,c);              \
+            d += temp1; h = temp1 + temp2;          \
+        }
+
+	A = ctx->state[0];
+	B = ctx->state[1];
+	C = ctx->state[2];
+	D = ctx->state[3];
+	E = ctx->state[4];
+	F = ctx->state[5];
+	G = ctx->state[6];
+	H = ctx->state[7];
+
+	P(A, B, C, D, E, F, G, H, W[0], 0x428A2F98);
+	P(H, A, B, C, D, E, F, G, W[1], 0x71374491);
+	P(G, H, A, B, C, D, E, F, W[2], 0xB5C0FBCF);
+	P(F, G, H, A, B, C, D, E, W[3], 0xE9B5DBA5);
+	P(E, F, G, H, A, B, C, D, W[4], 0x3956C25B);
+	P(D, E, F, G, H, A, B, C, W[5], 0x59F111F1);
+	P(C, D, E, F, G, H, A, B, W[6], 0x923F82A4);
+	P(B, C, D, E, F, G, H, A, W[7], 0xAB1C5ED5);
+	P(A, B, C, D, E, F, G, H, W[8], 0xD807AA98);
+	P(H, A, B, C, D, E, F, G, W[9], 0x12835B01);
+	P(G, H, A, B, C, D, E, F, W[10], 0x243185BE);
+	P(F, G, H, A, B, C, D, E, W[11], 0x550C7DC3);
+	P(E, F, G, H, A, B, C, D, W[12], 0x72BE5D74);
+	P(D, E, F, G, H, A, B, C, W[13], 0x80DEB1FE);
+	P(C, D, E, F, G, H, A, B, W[14], 0x9BDC06A7);
+	P(B, C, D, E, F, G, H, A, W[15], 0xC19BF174);
+	P(A, B, C, D, E, F, G, H, R(16), 0xE49B69C1);
+	P(H, A, B, C, D, E, F, G, R(17), 0xEFBE4786);
+	P(G, H, A, B, C, D, E, F, R(18), 0x0FC19DC6);
+	P(F, G, H, A, B, C, D, E, R(19), 0x240CA1CC);
+	P(E, F, G, H, A, B, C, D, R(20), 0x2DE92C6F);
+	P(D, E, F, G, H, A, B, C, R(21), 0x4A7484AA);
+	P(C, D, E, F, G, H, A, B, R(22), 0x5CB0A9DC);
+	P(B, C, D, E, F, G, H, A, R(23), 0x76F988DA);
+	P(A, B, C, D, E, F, G, H, R(24), 0x983E5152);
+	P(H, A, B, C, D, E, F, G, R(25), 0xA831C66D);
+	P(G, H, A, B, C, D, E, F, R(26), 0xB00327C8);
+	P(F, G, H, A, B, C, D, E, R(27), 0xBF597FC7);
+	P(E, F, G, H, A, B, C, D, R(28), 0xC6E00BF3);
+	P(D, E, F, G, H, A, B, C, R(29), 0xD5A79147);
+	P(C, D, E, F, G, H, A, B, R(30), 0x06CA6351);
+	P(B, C, D, E, F, G, H, A, R(31), 0x14292967);
+	P(A, B, C, D, E, F, G, H, R(32), 0x27B70A85);
+	P(H, A, B, C, D, E, F, G, R(33), 0x2E1B2138);
+	P(G, H, A, B, C, D, E, F, R(34), 0x4D2C6DFC);
+	P(F, G, H, A, B, C, D, E, R(35), 0x53380D13);
+	P(E, F, G, H, A, B, C, D, R(36), 0x650A7354);
+	P(D, E, F, G, H, A, B, C, R(37), 0x766A0ABB);
+	P(C, D, E, F, G, H, A, B, R(38), 0x81C2C92E);
+	P(B, C, D, E, F, G, H, A, R(39), 0x92722C85);
+	P(A, B, C, D, E, F, G, H, R(40), 0xA2BFE8A1);
+	P(H, A, B, C, D, E, F, G, R(41), 0xA81A664B);
+	P(G, H, A, B, C, D, E, F, R(42), 0xC24B8B70);
+	P(F, G, H, A, B, C, D, E, R(43), 0xC76C51A3);
+	P(E, F, G, H, A, B, C, D, R(44), 0xD192E819);
+	P(D, E, F, G, H, A, B, C, R(45), 0xD6990624);
+	P(C, D, E, F, G, H, A, B, R(46), 0xF40E3585);
+	P(B, C, D, E, F, G, H, A, R(47), 0x106AA070);
+	P(A, B, C, D, E, F, G, H, R(48), 0x19A4C116);
+	P(H, A, B, C, D, E, F, G, R(49), 0x1E376C08);
+	P(G, H, A, B, C, D, E, F, R(50), 0x2748774C);
+	P(F, G, H, A, B, C, D, E, R(51), 0x34B0BCB5);
+	P(E, F, G, H, A, B, C, D, R(52), 0x391C0CB3);
+	P(D, E, F, G, H, A, B, C, R(53), 0x4ED8AA4A);
+	P(C, D, E, F, G, H, A, B, R(54), 0x5B9CCA4F);
+	P(B, C, D, E, F, G, H, A, R(55), 0x682E6FF3);
+	P(A, B, C, D, E, F, G, H, R(56), 0x748F82EE);
+	P(H, A, B, C, D, E, F, G, R(57), 0x78A5636F);
+	P(G, H, A, B, C, D, E, F, R(58), 0x84C87814);
+	P(F, G, H, A, B, C, D, E, R(59), 0x8CC70208);
+	P(E, F, G, H, A, B, C, D, R(60), 0x90BEFFFA);
+	P(D, E, F, G, H, A, B, C, R(61), 0xA4506CEB);
+	P(C, D, E, F, G, H, A, B, R(62), 0xBEF9A3F7);
+	P(B, C, D, E, F, G, H, A, R(63), 0xC67178F2);
+
+	ctx->state[0] += A;
+	ctx->state[1] += B;
+	ctx->state[2] += C;
+	ctx->state[3] += D;
+	ctx->state[4] += E;
+	ctx->state[5] += F;
+	ctx->state[6] += G;
+	ctx->state[7] += H;
+    
+    #undef P
+    #undef R
+    #undef F1
+    #undef F0
+    #undef S3
+    #undef S2
+    #undef S1
+    #undef S0
+    #undef ROTR
+    #undef SHR
+}
+
+static void sha256_update(sha256_context * ctx, uint8_t * input, uint32_t length) {
+	uint32_t left, fill;
+
+	if (!length)
+		return;
+
+	left = ctx->total[0] & 0x3F;
+	fill = 64 - left;
+
+	ctx->total[0] += length;
+	ctx->total[0] &= 0xFFFFFFFF;
+
+	if (ctx->total[0] < length)
+		ctx->total[1]++;
+
+	if (left && length >= fill) {
+		memcpy((void *) (ctx->buffer + left), (void *) input, fill);
+		sha256_process(ctx, ctx->buffer);
+		length -= fill;
+		input += fill;
+		left = 0;
+	}
+
+	while (length >= 64) {
+		sha256_process(ctx, input);
+		length -= 64;
+		input += 64;
+	}
+
+	if (length)
+		memcpy((void *) (ctx->buffer + left), (void *) input, length);
+}
+
+static uint8_t sha256_padding[64] = {
+	0x80, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+	   0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+	   0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+	   0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+};
+
+static void sha256_finish(sha256_context * ctx, uint8_t digest[32]) {
+	uint32_t last, padn;
+	uint32_t high, low;
+	uint8_t msglen[8];
+
+	high = ((ctx->total[0] >> 29)
+		| (ctx->total[1] << 3));
+	low = (ctx->total[0] << 3);
+
+	PUT_UINT32_BE(high, msglen, 0);
+	PUT_UINT32_BE(low, msglen, 4);
+
+	last = ctx->total[0] & 0x3F;
+	padn = (last < 56) ? (56 - last) : (120 - last);
+
+	sha256_update(ctx, sha256_padding, padn);
+	sha256_update(ctx, msglen, 8);
+
+	PUT_UINT32_BE(ctx->state[0], digest, 0);
+	PUT_UINT32_BE(ctx->state[1], digest, 4);
+	PUT_UINT32_BE(ctx->state[2], digest, 8);
+	PUT_UINT32_BE(ctx->state[3], digest, 12);
+	PUT_UINT32_BE(ctx->state[4], digest, 16);
+	PUT_UINT32_BE(ctx->state[5], digest, 20);
+	PUT_UINT32_BE(ctx->state[6], digest, 24);
+	PUT_UINT32_BE(ctx->state[7], digest, 28);
+}
+
+/* END SHA-256 IMPLEMENTATION */
 
 /* an overcomplicated way to indent opt flags, at least it only uses a single buffer w/o resizing */
 static size_t indent_opts_labelsize(void);
@@ -117,17 +384,18 @@ static bool parse(FILE* source, FILE* dest, bool strip);
 
 static bool handle_target_set(char** set, size_t nset);
 
-static bool help_mode = false,  /* if true, the help will be displayed and iheaders will exit */
-    verbose_mode = false,       /* if true, extra information will be displayed during processing */
-    pipe_mode    = false,       /* pipe the output will be piped to stdout */
-    gaurd_mode   = false,       /* place include gaurds in generated header files */
-    merge_mode   = false,       /* merge the results into one header */
-    strip_mode   = false;       /* strip mode, instead of extracting header code */
+static bool help_mode = false, /* if true, the help will be displayed and iheaders will exit     */
+    verbose_mode  = false,     /* if true, extra information will be displayed during processing */
+    pipe_mode     = false,     /* pipe the output will be piped to stdout                        */
+    gaurd_mode    = false,     /* place include gaurds in generated header files                 */
+    merge_mode    = false,     /* merge the results into one header                              */
+    strip_mode    = false,     /* strip mode, instead of extracting header code                  */
+    skip_checksum = false;     /* skip performing checksums on file outputs                      */
 
 static const char* token = "@", /* token to use in processing */
-    * header_dir    = NULL,     /* output header directory */
-    * root_dir      = NULL,     /* root source directory */
-    * single_target = NULL,     /* single output header file */
+    * header_dir    = NULL,     /* output header directory    */
+    * root_dir      = NULL,     /* root source directory      */
+    * single_target = NULL,     /* single output header file  */
     * target_prefix = "",
     * target_suffix = "";
 
@@ -183,6 +451,9 @@ int main(int argc, char** argv) {
             break;
         case 'I':
             indent_tab_size = atoi(optarg);
+            break;
+        case 'K':
+            skip_checksum = true;
             break;
         case 'O':
             merge_mode = true;
@@ -924,16 +1195,93 @@ static bool handle_open(char* source, char* dest) {
     }
     FILE* fsource = fopen(source, "r");
     FOPEN_CHECK(source);
-    FILE* fdest = fopen(dest, "w");
+    int fddest = open(dest, O_CREAT | O_RDWR, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
     FOPEN_CHECK(dest);
-    if (gaurd_mode && !strip_mode) {
-        struct timespec spec;
-        clock_gettime(CLOCK_REALTIME, &spec);
-        fprintf(fdest, "\n#ifndef gen_%d_%ld\n#define gen_%d_%ld\n",
-                (int) spec.tv_sec, spec.tv_nsec, (int) spec.tv_sec, spec.tv_nsec);
+    FILE* fdest = fdopen(fddest, "r+");
+    FOPEN_CHECK(dest);
+    // int fddest = fileno(fdest);
+    if (fddest == -1)
+        ERRNO_CHECK("error while getting descriptor for file stream", dest);
+
+    /* Allocate space for checksum work */
+    struct stat attrib;
+    sha256_context ctx;
+    uint8_t buf[4096];
+    uint8_t old_digest[32];
+    uint8_t new_digest[32];
+    int i;
+
+    if (!strip_mode) {
+        if (!skip_checksum) {
+            if (fstat(fddest, &attrib) != 0)
+                ERRNO_CHECK("error while trying to stat destination file", dest);
+            
+            fseek(fdest, gaurd_mode ? 66 : 0, SEEK_SET); /* reset FILE* index */
+        
+            /* Perform checksum on the destination */
+            sha256_starts(&ctx);
+            while ((i = fread(buf, 1, sizeof(buf), fdest)) > 0)
+                sha256_update(&ctx, buf, i);
+            sha256_finish(&ctx, old_digest);
+        
+            fseek(fdest, 0, SEEK_SET); /* reset FILE* index */
+        }
+        
+        if (gaurd_mode) {
+            struct timespec spec;
+            clock_gettime(CLOCK_REALTIME, &spec);
+            fprintf(fdest, "\n#ifndef gen_%09d_%09ld\n#define gen_%09d_%09ld\n",
+                    (int) spec.tv_sec, spec.tv_nsec, (int) spec.tv_sec, spec.tv_nsec);
+        }
     }
+    
     bool ret = parse(fsource, fdest, strip_mode);
-    if (gaurd_mode && !strip_mode) fputs("\n#endif\n", fdest);
+    
+    if (!strip_mode) {
+        if (gaurd_mode)
+            fputs("\n#endif\n", fdest);
+        
+        fflush(fdest);
+        ftruncate(fddest, ftell(fdest));
+        
+        if (!skip_checksum) {
+            fseek(fdest, gaurd_mode ? 66 : 0, SEEK_SET); /* reset FILE* index */
+        
+            /* Perform checksum on the result */
+            sha256_starts(&ctx);
+            while ((i = fread(buf, 1, sizeof(buf), fdest)) > 0)
+                sha256_update(&ctx, buf, i);
+            sha256_finish(&ctx, new_digest);
+    
+            /* Compare checksums */
+            for (i = 0; i < 32; ++i) {
+                if (old_digest[i] != new_digest[i])
+                    break;
+            }
+    
+            if (i == 32) {
+                /* restore access/modification */
+                struct timespec set[2] = { attrib.st_atim, attrib.st_mtim }; 
+                if (futimens(fddest, set) == -1)
+                    ERRNO_CHECK("error white trying to restore modification and access"
+                                " times for destination file", dest);
+                if (verbose_mode)
+                    printf("'%s': unmodified (old = ", dest);
+            } else if (verbose_mode)
+                printf("'%s': modified (old = ", dest);
+            if (verbose_mode) {
+                for (i = 0; i < 32; ++i)
+                    printf("%02x", old_digest[i]);
+
+                fputs(", new = ", stdout);
+        
+                for (i = 0; i < 32; ++i)
+                    printf("%02x", new_digest[i]);
+
+                fputs(")\n", stdout);
+            }
+        }
+    }
     fclose(fsource);
     fclose(fdest);
     return ret;
